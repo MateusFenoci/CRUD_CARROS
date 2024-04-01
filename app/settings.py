8ef4adaf6f7112209876376ef1ad47194fca5dc5
MateusFenoci <85255548+MateusFenoci@users.noreply.github.com>
2024-04-01 20:01:03 -0300

    turning debug mode as false

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-mn0^zc%-$(1)irl%xa2@hzxs7#q=q5akapn(7!ix=kt5mu$c47"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
